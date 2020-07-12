ca2805b18d3c10e5a4788fb60828dbf265ef31e6
Huy Truong <huy.truong@mevry.com>
2020-07-12 22:49:00 +0700

    innit commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from './components/App'
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
